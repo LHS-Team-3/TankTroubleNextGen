7b29cb0e3fd3f3114185d93e8bdc6f80afaee632
GoogleBen <googleben@sbcglobal.net>
2015-05-06 17:01:59 -0500

    test

---
+++ src/com/lhs/game/Bullet.java
@@ -0,0 +1,7 @@
+package com.lhs.game;
+
+public class Bullet {
+	
+	//TODO: make bullet class (contains info for what damage it does and how it does it)
+	
+}
---
+++ src/com/lhs/game/Tank.java
@@ -0,0 +1,13 @@
+package com.lhs.game;
+
+import java.awt.*;
+
+public class Tank {
+	
+	public Color color;
+	public String name;
+	public Bullet shot;
+	
+	//TODO: make actual tank class (must get input)
+	
+}
